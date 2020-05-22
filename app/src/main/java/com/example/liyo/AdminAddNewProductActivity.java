da5f55cc22c91f69ec6b0f9c09370534239fd509
SHUBHAM <thakurshubhamsagar062@gmail.com>
2020-05-22 09:14:24 +0530

    Initial commit

---
+++ app/src/main/java/com/example/liyo/AdminAddNewProductActivity.java
@@ -0,0 +1,21 @@
+package com.example.liyo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.icu.util.ULocale;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class AdminAddNewProductActivity extends AppCompatActivity {
+     private String CategoryName;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_admin_add_new_product );
+
+
+        CategoryName = getIntent().getExtras().get("category").toString();
+        Toast.makeText( this, CategoryName,Toast.LENGTH_SHORT ).show();
+    }
+}
